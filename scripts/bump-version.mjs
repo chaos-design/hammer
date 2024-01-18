2aca4f49da445bc97a6590edc08d1370bccccb44
zhongchao.xcz <1085131904@qq.com>
2024-01-18 21:14:58 +0800

    chore: ci

--- scripts/bump-version.mjs
+++ scripts/bump-version.mjs
@@ -16,39 +16,39 @@ console.log(`execSync ${bv_cmd}`);
 
 execSync(bv_cmd, { stdio: 'inherit' });
 
-await fs.ensureFile('CHANGELOG.md');
+// await fs.ensureFile('CHANGELOG.md');
 
-let newChangelog = '';
+// let newChangelog = '';
 
-try {
-  const gitRes = await $`git log --pretty=format:"- %s" "main"...HEAD`;
-  newChangelog = gitRes.stdout.trim();
-} catch {
-  console.error(
-    chalk.redBright('Could not get git log, please write changelog manually.'),
-  );
-}
+// try {
+//   const gitRes = await $`git log --pretty=format:"- %s" "main"...HEAD`;
+//   newChangelog = gitRes.stdout.trim();
+// } catch {
+//   console.error(
+//     chalk.redBright('Could not get git log, please write changelog manually.'),
+//   );
+// }
 
-const now = new Date();
-const currentDate = `${now.getFullYear()}-${
-  now.getMonth() + 1
-}-${now.getDate()}`;
-const title = `## ${version} (${currentDate})`;
+// const now = new Date();
+// const currentDate = `${now.getFullYear()}-${
+//   now.getMonth() + 1
+// }-${now.getDate()}`;
+// const title = `## ${version} (${currentDate})`;
 
-await fs.ensureFile('CHANGELOG.md');
-const oldChangelog = await fs.readFile('CHANGELOG.md', 'utf8');
-const changelog = [title, newChangelog, oldChangelog]
-  .filter((item) => !!item)
-  .join('\n\n');
+// await fs.ensureFile('CHANGELOG.md');
+// const oldChangelog = await fs.readFile('CHANGELOG.md', 'utf8');
+// const changelog = [title, newChangelog, oldChangelog]
+//   .filter((item) => !!item)
+//   .join('\n\n');
 
-await fs.writeFile('CHANGELOG.md', changelog);
+// await fs.writeFile('CHANGELOG.md', changelog);
 
-console.log(
-  `\nNow you can make adjustments to ${chalk.cyan(
-    'CHANGELOG.md',
-  )}. Then press enter to continue.`,
-);
+// console.log(
+//   `\nNow you can make adjustments to ${chalk.cyan(
+//     'CHANGELOG.md',
+//   )}. Then press enter to continue.`,
+// );
 
-process.stdin.setRawMode(true);
-process.stdin.resume();
-process.stdin.on('data', process.exit.bind(process, 0));
+// process.stdin.setRawMode(true);
+// process.stdin.resume();
+// process.stdin.on('data', process.exit.bind(process, 0));

33334e1416536db4dad7e97ad1e95b167c1a8614
zhongchao.xcz <1085131904@qq.com>
2024-01-19 12:34:56 +0800

    chore: ci;

--- scripts/bump-version.mjs
+++ scripts/bump-version.mjs
@@ -6,49 +6,28 @@ import { $, chalk, fs } from 'zx';
 const { getChangedPackages } = require('@chaos-design/utils-pkg');
 const changedPackages = getChangedPackages();
 
-// const { version } = await fs.readJSON('package.json');
 const changed = changedPackages.map((c) => c.path).join(' ');
 const bv_cmd = `bumpp -r ${changed} --quiet`;
 
-// await fs.writeFileSync('version', version);
+const build = (changedPackages || [])
+  .map((item) => {
+    const pathArray = item.path.split('/');
+    pathArray.pop();
+
+    return pathArray.join('/');
+  })
+  .filter(Boolean)
+  .map((item) => `--filter="${item}"`);
+
+const buildConfig = `#!/bin/bash
+
+pnpm -r ${build.join(' ')}  exec npm publish --access public --no-git-checks
+`;
+
+await fs.writeFileSync('./scripts/publish-to-npm.sh', buildConfig);
+
+execSync('chmod +x ./scripts/publish-to-npm.sh');
 
 console.log(`execSync ${bv_cmd}`);
 
 execSync(bv_cmd, { stdio: 'inherit' });
-
-// await fs.ensureFile('CHANGELOG.md');
-
-// let newChangelog = '';
-
-// try {
-//   const gitRes = await $`git log --pretty=format:"- %s" "main"...HEAD`;
-//   newChangelog = gitRes.stdout.trim();
-// } catch {
-//   console.error(
-//     chalk.redBright('Could not get git log, please write changelog manually.'),
-//   );
-// }
-
-// const now = new Date();
-// const currentDate = `${now.getFullYear()}-${
-//   now.getMonth() + 1
-// }-${now.getDate()}`;
-// const title = `## ${version} (${currentDate})`;
-
-// await fs.ensureFile('CHANGELOG.md');
-// const oldChangelog = await fs.readFile('CHANGELOG.md', 'utf8');
-// const changelog = [title, newChangelog, oldChangelog]
-//   .filter((item) => !!item)
-//   .join('\n\n');
-
-// await fs.writeFile('CHANGELOG.md', changelog);
-
-// console.log(
-//   `\nNow you can make adjustments to ${chalk.cyan(
-//     'CHANGELOG.md',
-//   )}. Then press enter to continue.`,
-// );
-
-// process.stdin.setRawMode(true);
-// process.stdin.resume();
-// process.stdin.on('data', process.exit.bind(process, 0));

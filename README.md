29725f6075b77eb9c9dfdb4e9b17e9fbe4e3618b
zhongchao.xcz <1085131904@qq.com>
2024-09-23 20:19:54 +0800

    feat: classnames

--- README.md
+++ README.md
@@ -4,6 +4,15 @@
 
 [More Information](./packages.md)
 
+## Development
+
+### Publish
+
+```sh
+# npm / yarn
+pnpm run release
+```
+
 ## License
 
 [MIT](./LICENSE) License &copy; 2023-PRESENT [chaos-design](https://github.com/chaos-design)
---
+++ packages/utils/classnames/src/index.ts
@@ -0,0 +1,46 @@
+// LINK_TO: https://github.com/JedWatson/classnames
+const hasOwn = {}.hasOwnProperty;
+
+function prefix(name: string = ''): (...args: any[]) => string {
+  return (...args: any): string => {
+    const classes: string[] = [];
+
+    for (let i = 0; i < args.length; i++) {
+      const arg = args[i];
+
+      if (!arg) {
+        continue;
+      }
+
+      const argType = typeof arg;
+
+      if (argType === 'string' || argType === 'number') {
+        classes.push(arg);
+      } else if (Array.isArray(arg)) {
+        if (arg.length) {
+          // eslint-disable-next-line prefer-spread
+          const inner = prefix(name).apply(null, arg);
+          inner && classes.push(inner);
+        }
+      } else if (argType === 'object') {
+        if (arg.toString !== Object.prototype.toString) {
+          classes.push(arg.toString());
+        } else {
+          for (const key in arg) {
+            if (hasOwn.call(arg, key) && arg[key]) {
+              classes.push(key);
+            }
+          }
+        }
+      }
+    }
+
+    return classes.map((c) => (`${c}`.includes(name) ? c : name + c)).join(' ');
+  };
+}
+
+const classnames = prefix();
+
+export { classnames, prefix };
+
+export default classnames;
